1b1ee0796fbd8433544f291d5d9f441415400302
Julio Gabriel Chilela <juliochilela@hotmail.com>
2016-08-16 15:18:21 -0500

    Initial Commit

---
+++ Git Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git Demo
+//
+//  Created by Júlio Gabriel Chilela on 8/16/16.
+//  Copyright © 2016 Júlio Gabriel Chilela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
